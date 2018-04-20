a24a172adec3683845acedf79144149552512af5
AlySalah96 <you@example.com>
2018-04-20 18:26:23 +0200

    second cpmmit

--- app/Product.php
+++ app/Product.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     //
+    
     public function supliersOf()
     {
         return $this->belongsToMany('App\Suplier','supliers_products_relations','prod_id','sup_id');
